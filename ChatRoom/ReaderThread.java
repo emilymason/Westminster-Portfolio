da76656ac1b9d7394930d5ced024782add82596e
Emily Mason <33332250+emilymason@users.noreply.github.com>
2018-09-24 17:12:36 -0600

    Add files via upload

--- ChatRoom/ReaderThread.java
+++ ChatRoom/ReaderThread.java
@@ -1,17 +1,75 @@
-public class ReaderThread implements Runnable 
-{ 
-    public void run() { 
-        try { 
-                // get the input stream from the socket
-                while(true) { 
-                    // read from the socket
-                    /** 
-                     * ok, data has now arrived. Display it in the text area, 
-                     * and resume listening from the socket. 
-                     */ 
-                } 
-        }
-        catch (Exception e) {}
-//        catch (java.io.IOException ioe) { }
-    }
+/**
+ * This thread is passed a socket that it reads from. Whenever it gets input
+ * it writes it to the ChatScreen text area using the displayMessage() method.
+ */
+
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import dealios.ChatroomBroadcast;
+import dealios.ChatroomEnd;
+import dealios.ChatroomResponse;
+import dealios.ChatroomUpdate;
+
+public class ReaderThread implements Runnable
+{
+	Socket server;
+	BufferedReader fromServer;
+	ChatScreen screen;
+	String message = null;
+
+	public ReaderThread(Socket server, ChatScreen screen) {
+		this.server = server;
+		this.screen = screen;
+	}
+
+	public void run() {
+		
+		try {
+			fromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
+			
+		  	
+			
+			while((message = fromServer.readLine()) != null){
+			
+				System.out.println(message);
+				
+				if(message.contains("chatroom-response")){
+					Gson gson = new GsonBuilder().create();
+					
+					ChatroomResponse cr = gson.fromJson(message, ChatroomResponse.class);
+					ChatScreen.myUsername = ChatScreen.myUsername + ":" + cr.userID;
+					
+				}
+				
+				else if (message.contains("chatroom-broadcast")){
+					Gson gson = new GsonBuilder().create();
+					
+					ChatroomBroadcast cb = gson.fromJson(message, ChatroomBroadcast.class);
+					screen.displayMessage(cb.message);
+					
+				}
+				
+				else if(message.contains("chatroom-update")){
+					Gson gson = new GsonBuilder().create();
+					
+					ChatroomUpdate cu = gson.fromJson(message, ChatroomUpdate.class);
+					
+					screen.displayMessage(cu.id + "  " + cu.type_of_update);
+				}
+
+			
+			
+			
+			}
+		}
+		
+		catch (IOException ioe) { ioe.printStackTrace(); }
+
+	
+	}
 }
